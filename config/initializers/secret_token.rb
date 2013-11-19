d54102c909b2b321eb80d8f6853f814f07ae102a
Karen Caplan <keaslk@gmail.com>
2013-11-19 15:59:04 -0800

    removed files manually

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-IrisDnb::Application.config.secret_token = 'ce5022d13c42a1805cb9dab3da9c998ac54d9c9789c7b34f476ba9d73fc54fe377c18e912559cd36ef9b9cc65fc26f214da3143275f9564b13cd3ebd4e96d594'
