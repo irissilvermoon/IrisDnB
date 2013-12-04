221791c4c3105734fc3171cdc46c790e3d599861
Karen Caplan <keaslk@gmail.com>
2013-12-03 19:12:07 -0800

    added js to close content boxes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,8 @@
 
-
-
-
 $(document).ready(function() {
   $('h1').on('click', function(event) {
     var $h1 = $(event.currentTarget)
+    $('.content').not($h1.next()).hide("slow")
     $h1.next().slideToggle("slow")
   });
 });
